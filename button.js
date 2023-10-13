e474d10e7a26049ddd77a9684af0c7888e5c54ef
rockyhenderson <jack@teamhendo.com>
2023-10-13 14:34:09 +0100

    button being weird

---
+++ button.js
@@ -0,0 +1,9 @@
+document.getElementById("product-main-text").style.display = "none";
+
+
+function mainContent(){
+    $("#product-main-text").show()
+    $("#product-intro-text").hide()
+}
+function removeMainContet(){
+}
